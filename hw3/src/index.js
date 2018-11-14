5b5cd4a2c91a466bba64eba8c63bb4b26b41c37b
chuyuxuan123 <chuyuxuan@126.com>
2018-11-14 21:16:59 +0800

    remove some unless file creates by the frame

--- hw3/src/index.js
+++ hw3/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Todoapp from './component/Todoapp';
+import ToDoapp from './component/ToDoapp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Todoapp />, document.getElementById('root'));
+ReactDOM.render(<ToDoapp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
