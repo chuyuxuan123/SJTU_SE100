0d4ea45dceabd5b647070162cb33dfce56476757
chuyuxuan123 <chuyuxuan@126.com>
2018-11-25 17:32:33 +0800

    update unit test

--- hw3/src/test/ToDoapp.test.js
+++ hw3/src/test/ToDoapp.test.js
@@ -5,21 +5,39 @@ import { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import ToDoapp from '../component/ToDoapp';
+import ToDoForm from '../component/ToDoForm';
+import ToDoList from '../component/ToDoList';
+import ToDoItem from '../component/ToDoItem';
 
 configure({ adapter: new Adapter() });
 
-describe("test a react app", () => {
-    it("todoapp test", () => {
-        const testCom = shallow(<ToDoapp />);
-        expect(testCom.isEmpty()).toEqual(false);
+describe("<ToDoapp />", () => {
+    const wrapper = mount(<ToDoapp />);
+    it("title equal 'ToDo你想做什么?'",() => {
+        expect(wrapper.find("h1").text()).toEqual("ToDo你要做什么？");
     });
 
-    it("todoapp state test", () => {
-        const testCom = mount(<ToDoapp />);
-        expect(testCom.state['todos']).toEqual(undefined);
-        testCom.simulate('change', { target: { value: "helloworld" } });
-        testCom.find('.form-horizontal .form-group .col-sm-2 .btn-primary').simulate("click");
-        expect(testCom.state['todos']).toEqual(undefined);
-        testCom.unmount();
+    it("is a react component", () => {
+        expect(wrapper.exists()).toEqual(true);
     });
+
+    it("contains a ToDoFrom",() => {
+        expect(wrapper.containsMatchingElement(<ToDoForm />)).toEqual(true);
+    });
+
+    it("contains a ToDoList",() => {
+        expect(wrapper.containsMatchingElement(<ToDoList />)).toEqual(true);
+    });
+
+    it("state of array todos is empty", () => {
+        expect(wrapper.state['todos']).toEqual(undefined);
+    });
+
+    describe("<ToDoList />", () => {
+        it("is a react component", () => {
+            const childWrapper = wrapper.find(ToDoList);
+            expect(childWrapper.exists()).toEqual(true);
+        });
+    });
+
 });
